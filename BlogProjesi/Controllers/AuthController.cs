ebd27a124a049e925e3c7e65d87980ac1edd7c69
mert özdemir <mertozdemir91@hotmail.com>
2021-11-08 21:00:03 +0300

    ViewModellar olusturuldu

---
+++ BlogProjesi/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using BlogProjesi.Models.data;
+using BlogProjesi.Models.entity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjesi.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly UygulamaDbContext _db;
+
+        public AuthController(UygulamaDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost, ValidateAntiForgeryToken]
+
+        public IActionResult Login(User user) 
+        {
+            return View();
+        }
+    }
+}
